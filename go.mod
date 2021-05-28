e383d18e7fb31cf9177baf4ed7c25b68c719879c
Shogo Ohta <athos0220@gmail.com>
2021-05-29 01:13:00 +0900

    Add command line args to specify server host/port

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/athos/trenchman
 go 1.16
 
 require (
+	github.com/alecthomas/kong v0.2.16
 	github.com/fatih/color v1.12.0
 	github.com/google/uuid v1.2.0
 	github.com/stretchr/testify v1.7.0
--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"os"
 
+	"github.com/alecthomas/kong"
 	"github.com/athos/trenchman/trenchman/nrepl"
 	"github.com/athos/trenchman/trenchman/repl"
 )
 
+var opts struct {
+	Host string `name:"host" help:"host" default:"127.0.0.1"`
+	Port int `name:"port" required:"true" help:"port"`
+}
+
 func main() {
+	kong.Parse(&opts)
 	repl := repl.NewRepl(
 		os.Stdin, os.Stdout, os.Stderr,
 		func(ioHandler nrepl.IOHandler) *nrepl.Client {
-			client, err := nrepl.NewClient("127.0.0.1", 59800, ioHandler)
+			client, err := nrepl.NewClient(opts.Host, opts.Port, ioHandler)
 			if err != nil {
 				panic(err)
 			}
