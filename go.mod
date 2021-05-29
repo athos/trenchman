ca7462b12f954e53922f2bea90071b7c49bf4fa0
Shogo Ohta <athos0220@gmail.com>
2021-05-29 22:27:26 +0900

    Support auto-colorizing

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/kong v0.2.16
 	github.com/fatih/color v1.12.0
 	github.com/google/uuid v1.2.0
+	github.com/mattn/go-isatty v0.0.13
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 )
--- main.go
+++ main.go
@@ -7,18 +7,35 @@ import (
 	"github.com/alecthomas/kong"
 	"github.com/athos/trenchman/trenchman/nrepl"
 	"github.com/athos/trenchman/trenchman/repl"
+	"github.com/mattn/go-isatty"
 )
 
 const (
 	COLOR_NONE   = "none"
+	COLOR_AUTO   = "auto"
 	COLOR_ALWAYS = "always"
 )
 
 var opts struct {
-	Host  string `name:"host" help:"host" default:"127.0.0.1"`
-	Port  int    `name:"port" required:"true" help:"port"`
+	Host  string `name:"host" short:"H" help:"host" default:"127.0.0.1"`
+	Port  int    `name:"port" short:"p" required:"true" help:"port"`
 	Eval  string `name:"eval" short:"e" help:"eval"`
-	Color string `name:"color" short:"c" enum:"always,none" default:"always" help:"color"`
+	Color string `name:"color" short:"c" enum:"always,auto,none" default:"auto" help:"color"`
+}
+
+func colorized(color string) bool {
+	switch color {
+	case COLOR_NONE:
+		return false
+	case COLOR_ALWAYS:
+		return true
+	case COLOR_AUTO:
+		if isatty.IsTerminal(os.Stdout.Fd()) ||
+			isatty.IsCygwinTerminal(os.Stdout.Fd()) {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -29,7 +46,7 @@ func main() {
 		In:       os.Stdin,
 		Out:      os.Stdout,
 		Err:      os.Stderr,
-		Printer:  repl.NewPrinter(opts.Color != COLOR_NONE),
+		Printer:  repl.NewPrinter(colorized(opts.Color)),
 		HidesNil: oneshotEval,
 	}, func(ioHandler nrepl.IOHandler) *nrepl.Client {
 		client, err := nrepl.NewClient(opts.Host, opts.Port, ioHandler)
