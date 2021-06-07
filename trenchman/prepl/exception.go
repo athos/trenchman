214c82fca6ff6bc0d5818036ab68b9ddedd093a3
Shogo Ohta <athos0220@gmail.com>
2021-06-08 00:06:21 +0900

    WIP: Reconstruct exception for all phases

--- trenchman/prepl/exception.go
+++ trenchman/prepl/exception.go
@@ -2,6 +2,7 @@ package prepl
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 
 	"olympos.io/encoding/edn"
@@ -17,7 +18,7 @@ type (
 	viaEntry struct {
 		Type    edn.Symbol
 		Message string
-		// Data    map[edn.Keyword]interface{}
+		Data    map[edn.Keyword]interface{}
 	}
 
 	triageData struct {
@@ -40,18 +41,62 @@ func exTriage(ex *exception) *triageData {
 	td := triageData{phase: phase}
 	via := ex.Via
 	var typ, msg string
+	var topData map[edn.Keyword]interface{}
+	var source string
 	if len(via) > 0 {
 		lastEntry := via[len(via)-1]
 		typ = lastEntry.Type.String()
 		msg = lastEntry.Message
+		topData = via[0].Data
+		if src, ok := topData[edn.Keyword("clojure.error/source")]; ok {
+			source = src.(string)
+		}
 	}
 	switch phase {
 	case "read-source":
-		//TODO
+		if source != "" &&
+			source != "NO_SOURCE_FILE" &&
+			source != "NO_SOURCE_PATH" {
+			td.source = filepath.Base(source)
+			td.path = filepath.Dir(source)
+		}
+		if msg != "" {
+			td.cause = msg
+		}
 	case "compile-syntax-check", "compilation", "macro-syntax-check", "macroexpansion":
-		//TODO
+		mergeToTriageData(td, topData)
+		if source != "" &&
+			source != "NO_SOURCE_FILE" &&
+			source != "NO_SOURCE_PATH" {
+			td.source = filepath.Base(source)
+			td.path = filepath.Dir(source)
+		}
+		if typ != "" {
+			td.class = typ
+		}
+		if msg != "" {
+			td.cause = msg
+		}
 	case "read-eval-result", "print-eval-result":
-		//TODO
+		mergeToTriageData(td, topData)
+		if len(ex.Trace) > 0 {
+			source, method, file, line := coerceTraceEntry(ex.Trace[0])
+			if line != 0 {
+				td.line = line
+			}
+			if file != "" {
+				td.source = file
+			}
+			if source != "" && method != "" {
+				td.symbol = javaLocToSource(source, method)
+			}
+		}
+		if typ != "" {
+			td.class = typ
+		}
+		if msg != "" {
+			td.cause = msg
+		}
 	case "execution":
 		td.class = typ
 		if msg != "" {
@@ -67,12 +112,38 @@ func exTriage(ex *exception) *triageData {
 	return &td
 }
 
+func mergeToTriageData(td triageData, data map[edn.Keyword]interface{}) {
+	if data == nil {
+		return
+	}
+	if phase, ok := data[edn.Keyword("clojure.error/phase")]; ok {
+		td.phase = phase.(edn.Keyword).String()[1:]
+	}
+	if line, ok := data[edn.Keyword("clojure.error/line")]; ok {
+		td.line = int(line.(int64))
+	}
+	if column, ok := data[edn.Keyword("clojure.error/column")]; ok {
+		td.column = int(column.(int64))
+	}
+	if source, ok := data[edn.Keyword("clojure.error/source")]; ok {
+		td.source = source.(string)
+	}
+	if symbol, ok := data[edn.Keyword("clojure.error/symbol")]; ok {
+		td.symbol = symbol.(edn.Symbol).String()
+	}
+}
+
+func coerceTraceEntry(entry []interface{}) (string, string, string, int) {
+	source := entry[0].(edn.Symbol).String()
+	method := entry[1].(edn.Symbol).String()
+	file := entry[2].(string)
+	line := int(entry[3].(int64))
+	return source, method, file, line
+}
+
 func findFirstNonCoreEntry(trace [][]interface{}) (string, string, string, int, bool) {
 	for _, entry := range trace {
-		source := entry[0].(edn.Symbol).String()
-		method := entry[1].(edn.Symbol).String()
-		file := entry[2].(string)
-		line := int(entry[3].(int64))
+		source, method, file, line := coerceTraceEntry(entry)
 		if isCoreClass(source) {
 			return source, method, file, line, true
 		}
@@ -116,19 +187,56 @@ func exString(td *triageData) string {
 	}
 	switch td.phase {
 	case "read-source":
-		// TODO
+		return fmt.Sprintf(
+			"Syntax error reading source at (%s).\n%s",
+			loc, td.cause,
+		)
 	case "macro-syntax-check":
-		// TODO
+		symbol := td.symbol
+		if symbol != "" {
+			symbol += " "
+		}
+		return fmt.Sprintf(
+			"Syntax error macroexpanding %sat (%s).\n%s",
+			symbol, loc, td.cause,
+		)
 	case "macroexpansion":
-		// TODO
+		symbol := td.symbol
+		if symbol != "" {
+			symbol += " "
+		}
+		return fmt.Sprintf(
+			"Unexpected error%s macroexpanding %sat (%s).\n%s",
+			causeType, symbol, loc, td.cause,
+		)
 	case "compile-syntax-check":
-		// TODO
+		symbol := td.symbol
+		if symbol != "" {
+			symbol += " "
+		}
+		return fmt.Sprintf(
+			"Syntax error%s compiling %sat (%s).\n%s",
+			causeType, symbol, loc, td.cause,
+		)
 	case "compilation":
-		// TODO
+		symbol := td.symbol
+		if symbol != "" {
+			symbol += " "
+		}
+		return fmt.Sprintf(
+			"Unexpected error%s compiling %sat (%s).\n%s",
+			causeType, symbol, loc, td.cause,
+		)
 	case "read-eval-result":
-		// TODO
+		return fmt.Sprintf(
+			"Error reading eval result%s at %s (%s).\n%s",
+			causeType, td.symbol, loc, td.cause,
+		)
 	case "print-eval-result":
-		// TODO
+		return fmt.Sprintf(
+			"Error printing return value%s at %s (%s).\n%s",
+			causeType, td.symbol, loc, td.cause,
+		)
 	case "execution":
 		symbol := td.symbol
 		if symbol != "" {
