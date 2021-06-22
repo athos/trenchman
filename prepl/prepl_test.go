a82368f6a09f2e18f1ea1c73cc86cfbe21b5d312
Shogo Ohta <athos0220@gmail.com>
2021-06-23 00:32:31 +0900

    Add tests for prepl output handling

--- prepl/prepl_test.go
+++ prepl/prepl_test.go
@@ -17,8 +17,8 @@ type (
 	}
 
 	step struct {
-		expected string
-		response string
+		expected  string
+		responses []string
 	}
 )
 
@@ -28,10 +28,16 @@ func (m *mockServer) Read(b []byte) (int, error) {
 		<-(chan interface{}(nil))
 	}
 	step := &m.steps[0]
-	bs := []byte(step.response)
-	copy(b, bs)
+	var buf bytes.Buffer
+	for _, res := range step.responses {
+		buf.WriteString(res)
+	}
+	n, err := buf.Read(b)
+	if err != nil {
+		return 0, err
+	}
 	m.steps = m.steps[1:]
-	return len(bs), nil
+	return n, nil
 }
 
 func (m *mockServer) Write(b []byte) (int, error) {
@@ -58,43 +64,92 @@ func (m *mockServer) SetDeadline(t time.Time) error      { return nil }
 func (m *mockServer) SetReadDeadline(t time.Time) error  { return nil }
 func (m *mockServer) SetWriteDeadline(t time.Time) error { return nil }
 
+type mockOutputHandler struct {
+	outs []string
+	errs []string
+}
+
+func (m *mockOutputHandler) Out(s string) {
+	m.outs = append(m.outs, s)
+}
+
+func (m *mockOutputHandler) Err(s string) {
+	m.errs = append(m.errs, s)
+}
+
 type errorHandlerFunc func(error)
 
 func (f errorHandlerFunc) HandleErr(err error) {
 	f(err)
 }
 
-func setupMock(steps []step) *mockServer {
+func setupMock(steps ...step) *mockServer {
 	s := make([]step, 1, len(steps)+1)
 	s[0] = step{
 		"(set! *print-namespace-maps* false)",
-		`{:tag :ret, :val "nil"}`,
+		[]string{`{:tag :ret, :val "nil"}`},
 	}
 	s = append(s, steps...)
 	return &mockServer{s}
 }
 
 func TestEval(t *testing.T) {
-	mock := setupMock([]step{
+	tests := []struct {
+		input  string
+		step   step
+		result string
+		outs   []string
+		errs   []string
+	}{
 		{
-			"(+ 1 2)\n",
-			`{:tag :ret, :val "3"}`,
+			"(+ 1 2)",
+			step{
+				"(+ 1 2)\n",
+				[]string{`{:tag :ret, :val "3"}`},
+			},
+			"3",
+			nil,
+			nil,
 		},
-	})
-	var handledErr error
-	c, err := NewClient(&Opts{
-		connBuilder: func(_ string, _ int) (net.Conn, error) {
-			return mock, nil
+		{
+			"(run! prn (range 3))",
+			step{
+				"(run! prn (range 3))\n",
+				[]string{
+					`{:tag :out, :val "1"}`,
+					`{:tag :out, :val "2"}`,
+					`{:tag :out, :val "3"}`,
+					`{:tag :ret, :val "nil"}`,
+				},
+			},
+			"nil",
+			[]string{"1", "2", "3"},
+			nil,
 		},
-		ErrorHandler: errorHandlerFunc(func(err error) {
-			handledErr = err
-		}),
-	})
-	assert.Nil(t, err)
-	ch := c.Eval("(+ 1 2)")
-	ret := <-ch
-	assert.Equal(t, "3", ret.(string))
-	assert.Nil(t, handledErr)
-	err = c.Close()
-	assert.Nil(t, err)
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			mock := setupMock(tt.step)
+			var outHandler mockOutputHandler
+			var handledErr error
+			c, err := NewClient(&Opts{
+				connBuilder: func(_ string, _ int) (net.Conn, error) {
+					return mock, nil
+				},
+				OutputHandler: &outHandler,
+				ErrorHandler: errorHandlerFunc(func(err error) {
+					handledErr = err
+				}),
+			})
+			assert.Nil(t, err)
+			ch := c.Eval(tt.input)
+			ret := <-ch
+			assert.Equal(t, tt.result, ret.(string))
+			assert.Nil(t, handledErr)
+			assert.Equal(t, tt.outs, outHandler.outs)
+			assert.Equal(t, tt.errs, outHandler.errs)
+			err = c.Close()
+			assert.Nil(t, err)
+		})
+	}
 }
