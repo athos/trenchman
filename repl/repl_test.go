ab1c0710a31ed0f18952b55da911e3937176b9c1
Shogo Ohta <athos0220@gmail.com>
2021-07-13 22:58:05 +0900

    Add more test cases for repl to improve coverage

--- repl/repl_test.go
+++ repl/repl_test.go
@@ -75,7 +75,7 @@ type mockReader struct {
 
 func newMockReader(input string) *mockReader {
 	return &mockReader{
-		r: strings.NewReader(input),
+		r:  strings.NewReader(input),
 		ch: make(chan struct{}),
 	}
 }
@@ -105,16 +105,78 @@ func setupRepl(r *mockReader, c *mockClient) *Repl {
 }
 
 func TestRepl(t *testing.T) {
-	r := newMockReader("(+ 1 2)\n")
-	c := newMockClient(
-		step{"(+ 1 2)", func(ch chan<- client.EvalResult) {
-			ch <- "3"
-			r.Close()
-		}},
-	)
-	repl := setupRepl(r, c)
-	repl.Start()
-	assert.Equal(t, "user=> 3\nuser=> ", c.outs.String())
-	assert.Equal(t, "", c.errs.String())
-	repl.Close()
+	var repl *Repl
+	var c *mockClient
+	var r *mockReader
+	tests := []struct {
+		input string
+		step  step
+		outs  string
+		errs  string
+	}{
+		{
+			"(+ 1 2)\n",
+			step{"(+ 1 2)", func(ch chan<- client.EvalResult) {
+				ch <- "3"
+				r.Close()
+			}},
+			"user=> 3\nuser=> ",
+			"",
+		},
+		{
+			"\n42\n",
+			step{"42", func(ch chan<- client.EvalResult) {
+				ch <- "42"
+				r.Close()
+			}},
+			"user=> user=> 42\nuser=> ",
+			"",
+		},
+		{
+			":repl/quit\n",
+			step{},
+			"user=> ",
+			"",
+		},
+		{
+			"[1\n 2\n 3]\n",
+			step{"[1\n 2\n 3]", func(ch chan<- client.EvalResult) {
+				ch <- "[1 2 3]"
+				r.Close()
+			}},
+			"user=>   #_=>   #_=> [1 2 3]\nuser=> ",
+			"",
+		},
+		{
+			"(println \"Hello, World!\")\n",
+			step{"(println \"Hello, World!\")", func(ch chan<- client.EvalResult) {
+				repl.Out("Hello, World!\n")
+				ch <- "nil"
+				r.Close()
+			}},
+			"user=> Hello, World!\nnil\nuser=> ",
+			"",
+		},
+		{
+			"(/ 1 0)\n",
+			step{"(/ 1 0)", func(ch chan<- client.EvalResult) {
+				repl.Err("divide by zero\n")
+				ch <- client.NewRuntimeError("divide by zero")
+				r.Close()
+			}},
+			"user=> user=> ",
+			"divide by zero\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			r = newMockReader(tt.input)
+			c = newMockClient(tt.step)
+			repl = setupRepl(r, c)
+			repl.Start()
+			assert.Equal(t, tt.outs, c.outs.String())
+			assert.Equal(t, tt.errs, c.errs.String())
+			repl.Close()
+		})
+	}
 }
