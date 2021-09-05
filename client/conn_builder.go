6828785f6903bd06492624b54f6a22e0f684a8dc
Shogo Ohta <athos0220@gmail.com>
2021-09-05 19:43:53 +0900

    Implement conn builder with retry

--- client/conn_builder.go
+++ client/conn_builder.go
@@ -3,6 +3,7 @@ package client
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 type ConnBuilder interface {
@@ -22,4 +23,26 @@ type ConnBuilderFunc func() (net.Conn, error)
 
 func (f ConnBuilderFunc) Connect() (net.Conn, error) {
 	return f()
+}
+
+func NewRetryConnBuilder(connBuilder ConnBuilder, timeout time.Duration, interval time.Duration) ConnBuilder {
+	if interval > timeout {
+		interval = timeout
+	}
+	return ConnBuilderFunc(func() (conn net.Conn, err error) {
+		end := time.Now().Add(timeout)
+		for {
+			conn, err = connBuilder.Connect()
+			if err == nil {
+				return conn, nil
+			}
+			if time.Now().After(end) {
+				return
+			}
+			time.Sleep(interval)
+			if time.Now().After(end) {
+				return
+			}
+		}
+	})
 }
